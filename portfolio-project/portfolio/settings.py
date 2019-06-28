055613cb1b94d951f93ef920b1c37c31f114de62
Frankie <frankie@mezanet.com>
2019-06-27 23:06:12 -0700

    IoT Meme Project Update

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fe79)y1@1)yc_ux7s-*7@^*06spapdl7oyd_wr6%(vo&ylpl5k'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
